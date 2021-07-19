126126e79590d5c3a0c88e80cebd681a44df0843
dtbj <87176273+dtbj@users.noreply.github.com>
2021-07-19 19:01:53 +0530

    Update index.js

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,8 +1,6 @@
 import app from 'flarum/forum/app';
-import addmodal from './addmodal';
 import addsearch from './addsearch';
 
 app.initializers.add('talentbrick-forum-search-for-mobile-devices', () => {
-    addmodal();
     addsearch();
 });
