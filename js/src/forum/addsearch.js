6925bb985d32ba76b8929b44b964d68aac9c3d7b
dtbj <87176273+dtbj@users.noreply.github.com>
2021-07-19 18:51:27 +0530

    Update addsearch.js

--- js/src/forum/addsearch.js
+++ js/src/forum/addsearch.js
@@ -15,26 +15,3 @@ export default () => {
     );
   });
 };
-
-import Modal from 'flarum/common/components/Modal';
-
-export default class searchmodal extends Modal {
-  static isDismissible = true;
-
-  className() {
-    return 'searchmodal';
-  }
-  title() {
-    // Content to show in the modal's title bar
-    return <p>What is your question?</p>;
-  }
-
-  content() {
-    // Content to show in the modal's body
-return [
-items.add('search', Search.component({
-            state: app.search,
-        }), -100);
-];
-  }
-}
