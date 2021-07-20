f7cad30c839eca79a3360bf753341785cdaf5f72
dtbj <87176273+dtbj@users.noreply.github.com>
2021-07-20 10:23:32 +0530

    Update searchmodal.js

--- js/src/forum/searchmodal.js
+++ js/src/forum/searchmodal.js
@@ -1,35 +1,35 @@
 import Modal from 'flarum/common/components/Modal';
-import { extend } from 'flarum/common/extend';
 import ItemList from 'flarum/utils/ItemList';
 
 export default class searchmodal extends Modal {
-  static isDismissible = true;
+    static isDismissible = true;
 
-  className() {
-    return 'searchmodal';
-  }
-  title() {
-    // Content to show in the modal's title bar
-    return <p>What is your question?</p>;
-  }
+    className() {
+        return 'searchmodal';
+    }
+    title() {
+        // Content to show in the modal's title bar
+        return <p>What is your question?</p>;
+    }
 
-  content() {
-    // Content to show in the modal's body
+    content() {
+        // Content to show in the modal's body
 
-    
-      return (
-      <div className="Modal-body">
-        <div className="searchbar">
-          {this.fields().toArray()}
-        </div>
-      </div>
-    );
-     fields() {
-const items = new ItemList();
-items.add('search', Search.component({
+
+        return (
+            <div className="Modal-body">
+                <div className="searchbar">
+                    {this.fields().toArray()}
+                </div>
+            </div>
+        );
+    }
+    fields() {
+        const items = new ItemList();
+        items.add('search', Search.component({
             state: app.search,
         }), -100),
-    return items;
-  }
- }
+        return items;
+    }
+
 }
